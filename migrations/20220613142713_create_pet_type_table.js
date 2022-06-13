5863bf7d7fcf9ae4f10e101a84d7953d8442450b
Zachary Haines <hainesz47@gmail.com>
2022-06-13 15:26:52 -0400

    mvp

---
+++ migrations/20220613142713_create_pet_type_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('pet_type', table => {
+    table.increments();
+    table.string('name', 250) // specifies type, field name, and limit (i.e. character limit)
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('pet_type');
+};
